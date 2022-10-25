c70d74721ac6578a89b944d843abc6cb7b6688e4
wasif <wasifbdjsr@gmail.com>
2022-10-25 19:14:41 +0600

    added something

---
+++ src/Data/data.jsx
@@ -0,0 +1,31 @@
+import Group_1 from '../Images/Rainbow Chauffer/Group-1.png';
+import Group_2 from '../Images/Rainbow Chauffer/Group-2.png';
+import Group_3 from '../Images/Rainbow Chauffer/Group-3.png';
+import Group_4 from '../Images/Rainbow Chauffer/Group-4.png';
+
+const ServiceData = [
+    {
+        id: 1,
+        image: Group_1,
+        title: "Select Destination",
+        description: "Completes all the work associated with planning and processing"
+    },
+    {
+        id: 2,
+        image: Group_2,
+        title: "Select Destination",
+        description: "Completes all the work associated with planning and processing"
+    },
+    {
+        id: 3,
+        image: Group_3,
+        title: "Select Destination",
+        description: "Completes all the work associated with planning and processing"
+    },
+    {
+        id: 4,
+        image: Group_4,
+        title: "Select Destination",
+        description: "Completes all the work associated with planning and processing"
+    },
+]
--- src/Pages/Home/Service.jsx
+++ src/Pages/Home/Service.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import {ServiceData} from '../../Data'
 
 const Service = () => {
   return (
-    <div>Service</div>
+    <div>
+        <h1 className='font-semibold md:text-5xl text-3xl text-center text-dark font-sans'>Things you need <span className=' text-primary'>to do</span> </h1>
+
+        <p className="text-center text-text_inverted py-4">
+          Simply follow these four steps to book a ride
+        </p>
+    </div>
   )
 }
 
